dee843bf859441d4a4d824de1f90208ac521e9ee
ivanmikhaylov1 <ivanmikhaylov123@gmail.com>
2025-02-07 20:44:06 +0300

    Init commit

---
+++ src/main/java/org/example/mikhaylovivan2semester/Application.java
@@ -0,0 +1,13 @@
+package org.example.mikhaylovivan2semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
