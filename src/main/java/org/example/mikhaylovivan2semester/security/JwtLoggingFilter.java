d47378f791eb0c864f2cd22a32b780caa44d69f6
ivanmikhaylov1 <ivanmikhaylov123@gmail.com>
2025-02-23 23:14:38 +0300

    Исправлен openapi

--- src/main/java/org/example/mikhaylovivan2semester/security/JwtLoggingFilter.java
+++ src/main/java/org/example/mikhaylovivan2semester/security/JwtLoggingFilter.java
@@ -4,12 +4,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 public class JwtLoggingFilter extends OncePerRequestFilter {
   @Override
-  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+  protected void doFilterInternal(HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain)
       throws ServletException, IOException {
     String authHeader = request.getHeader("Authorization");
     if (authHeader != null) {
