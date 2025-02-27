4cca693e301b13ca6375a45fb2bebe9de82b5485
ivanmikhaylov1 <ivanmikhaylov123@gmail.com>
2025-02-27 14:26:22 +0300

    Убраны тесты с временной базой данных

--- src/test/java/org/example/e2e/EndToEndTest.java
+++ src/test/java/org/example/e2e/EndToEndTest.java
@@ -1,101 +1,101 @@
-package org.example.e2e;
-
-import org.example.mikhaylovivan2semester.Application;
-import org.example.mikhaylovivan2semester.dto.UserDTO;
-import org.example.mikhaylovivan2semester.entity.User;
-import org.example.mikhaylovivan2semester.repository.UserRepository;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.annotation.DirtiesContext;
-
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class, properties = "spring.config.location=classpath:/application-test.yml")
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-class EndToEndTest {
-  @Autowired
-  private UserRepository userRepository;
-
-  @Autowired
-  private TestRestTemplate restTemplate;
-
-  @LocalServerPort
-  private int port;
-
-  private String baseUrl;
-  private UUID userId;
-
-  @BeforeAll
-  void setUp() {
-    baseUrl = "http://localhost:" + port;
-    createTestUser();
-  }
-
-  private void createTestUser() {
-    User user = new User(null, "Test User", "testpassword");
-    User savedUser = userRepository.save(user);
-    userId = savedUser.getId();
-  }
-
-  @Test
-  void testSaveUser() {
-    Map<String, String> requestBody = new HashMap<>();
-    requestBody.put("name", "Test User");
-    requestBody.put("password", "testpassword");
-
-    ResponseEntity<String> response = restTemplate.postForEntity(
-        baseUrl + "/users",
-        requestBody,
-        String.class
-    );
-
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-  }
-
-  @Test
-  void testGetAllUsers() {
-    ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + "/users", String.class);
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-  }
-
-  @Test
-  void testGetUserById() {
-    ResponseEntity<UserDTO> response = restTemplate.getForEntity(baseUrl + "/users/" + userId, UserDTO.class);
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-  }
-
-  @Test
-  void testGetUserByName() {
-    ResponseEntity<String> response = restTemplate.getForEntity(
-        baseUrl + "/users/by-name?name=" + URLEncoder.encode("Test User", StandardCharsets.UTF_8),
-        String.class
-    );
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-  }
-
-  @Test
-  void testCheckIfUserExists() {
-    ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + "/users/exists?name=Test%20User", String.class);
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-  }
-
-  @Test
-  void testGetArticles() {
-    ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + "/articles", String.class);
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-  }
-}
+//package org.example.e2e;
+//
+//import org.example.mikhaylovivan2semester.Application;
+//import org.example.mikhaylovivan2semester.dto.UserDTO;
+//import org.example.mikhaylovivan2semester.entity.User;
+//import org.example.mikhaylovivan2semester.repository.UserRepository;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.TestInstance;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.web.server.LocalServerPort;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.test.annotation.DirtiesContext;
+//
+//import java.net.URLEncoder;
+//import java.nio.charset.StandardCharsets;
+//import java.util.HashMap;
+//import java.util.Map;
+//import java.util.UUID;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class, properties = "spring.config.location=classpath:/application-test.yml")
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+//class EndToEndTest {
+//  @Autowired
+//  private UserRepository userRepository;
+//
+//  @Autowired
+//  private TestRestTemplate restTemplate;
+//
+//  @LocalServerPort
+//  private int port;
+//
+//  private String baseUrl;
+//  private UUID userId;
+//
+//  @BeforeAll
+//  void setUp() {
+//    baseUrl = "http://localhost:" + port;
+//    createTestUser();
+//  }
+//
+//  private void createTestUser() {
+//    User user = new User(null, "Test User", "testpassword");
+//    User savedUser = userRepository.save(user);
+//    userId = savedUser.getId();
+//  }
+//
+//  @Test
+//  void testSaveUser() {
+//    Map<String, String> requestBody = new HashMap<>();
+//    requestBody.put("name", "Test User");
+//    requestBody.put("password", "testpassword");
+//
+//    ResponseEntity<String> response = restTemplate.postForEntity(
+//        baseUrl + "/users",
+//        requestBody,
+//        String.class
+//    );
+//
+//    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+//  }
+//
+//  @Test
+//  void testGetAllUsers() {
+//    ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + "/users", String.class);
+//    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//  }
+//
+//  @Test
+//  void testGetUserById() {
+//    ResponseEntity<UserDTO> response = restTemplate.getForEntity(baseUrl + "/users/" + userId, UserDTO.class);
+//    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//  }
+//
+//  @Test
+//  void testGetUserByName() {
+//    ResponseEntity<String> response = restTemplate.getForEntity(
+//        baseUrl + "/users/by-name?name=" + URLEncoder.encode("Test User", StandardCharsets.UTF_8),
+//        String.class
+//    );
+//    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//  }
+//
+//  @Test
+//  void testCheckIfUserExists() {
+//    ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + "/users/exists?name=Test%20User", String.class);
+//    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//  }
+//
+//  @Test
+//  void testGetArticles() {
+//    ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + "/articles", String.class);
+//    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//  }
+//}
--- src/test/java/org/example/mikhaylovivan2semester/controller/UserControllerTest.java
+++ src/test/java/org/example/mikhaylovivan2semester/controller/UserControllerTest.java
@@ -1,141 +1,141 @@
-package org.example.mikhaylovivan2semester.controller;
-
-import org.example.mikhaylovivan2semester.dto.UserDTO;
-import org.example.mikhaylovivan2semester.service.interfaces.UserService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-class UserControllerTest {
-  private MockMvc mockMvc;
-
-  @Mock
-  private UserService userService;
-
-  @InjectMocks
-  private UserController userController;
-
-  private UUID userId;
-  private UserDTO userDTO;
-
-  @BeforeEach
-  void setUp() {
-    MockitoAnnotations.openMocks(this);
-
-    mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
-
-    userId = UUID.randomUUID();
-    userDTO = new UserDTO(userId, "Test User");
-  }
-
-  @Test
-  void testFindAllUsers() throws Exception {
-    when(userService.findAll()).thenReturn(List.of(userDTO));
-
-    mockMvc.perform(get("/users"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.data[0].id").value(userDTO.id().toString()))
-        .andExpect(jsonPath("$.data[0].name").value(userDTO.name()));
-  }
-
-  @Test
-  void testSaveUser() throws Exception {
-    when(userService.save(anyString(), anyString())).thenReturn(java.util.Optional.of(userDTO));
-
-    mockMvc.perform(post("/users")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"name\":\"Test User\",\"password\":\"password123\"}"))
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("$.data.name").value("Test User"));
-
-    verify(userService, times(1)).save(anyString(), anyString());
-  }
-
-  @Test
-  void testSaveUserBadRequest() throws Exception {
-    when(userService.save(any(), any())).thenReturn(Optional.empty());
-
-    mockMvc.perform(post("/users")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"name\":\"Test User\",\"password\":\"password123\"}"))
-        .andExpect(status().isBadRequest())
-        .andExpect(jsonPath("$.message").value("Не удалось создать пользователя"));
-  }
-
-
-  @Test
-  void testGetUserById() throws Exception {
-    when(userService.getById(userId)).thenReturn(Optional.of(userDTO));
-
-    mockMvc.perform(get("/users/{userId}", userId))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.data.id").value(userDTO.id().toString()))
-        .andExpect(jsonPath("$.data.name").value(userDTO.name()));
-  }
-
-  @Test
-  void testGetUserByIdNotFound() throws Exception {
-    when(userService.getById(userId)).thenReturn(Optional.empty());
-
-    mockMvc.perform(get("/users/{userId}", userId))
-        .andExpect(status().isNotFound())
-        .andExpect(jsonPath("$.message").value("Пользователь не найден"));
-  }
-
-
-  @Test
-  void testFindUserByName() throws Exception {
-    when(userService.findByName("Test User")).thenReturn(Optional.of(userDTO));
-
-    mockMvc.perform(get("/users/by-name?name=Test User"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.data.id").value(userDTO.id().toString()))
-        .andExpect(jsonPath("$.data.name").value(userDTO.name()));
-  }
-
-  @Test
-  void testGetUserByNameNotFound() throws Exception {
-    when(userService.findByName("NonExistingUser")).thenReturn(java.util.Optional.empty());
-
-    mockMvc.perform(get("/users/by-name?name=NonExistingUser"))
-        .andExpect(status().isNotFound())
-        .andExpect(jsonPath("$.message").value("Пользователь не найден"));
-
-    verify(userService, times(1)).findByName("NonExistingUser");
-  }
-
-  @Test
-  void testUserExists() throws Exception {
-    when(userService.exists("Test User")).thenReturn(true);
-
-    mockMvc.perform(get("/users/exists?name=Test User"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.data").value(true));
-  }
-
-  @Test
-  void testUserNotExists() throws Exception {
-    when(userService.exists("Nonexistent User")).thenReturn(false);
-
-    mockMvc.perform(get("/users/exists?name=Nonexistent User"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.data").value(false));
-  }
-}
+//package org.example.mikhaylovivan2semester.controller;
+//
+//import org.example.mikhaylovivan2semester.dto.UserDTO;
+//import org.example.mikhaylovivan2semester.service.interfaces.UserService;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//
+//import java.util.List;
+//import java.util.Optional;
+//import java.util.UUID;
+//
+//import static org.mockito.ArgumentMatchers.any;
+//import static org.mockito.ArgumentMatchers.anyString;
+//import static org.mockito.Mockito.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//class UserControllerTest {
+//  private MockMvc mockMvc;
+//
+//  @Mock
+//  private UserService userService;
+//
+//  @InjectMocks
+//  private UserController userController;
+//
+//  private UUID userId;
+//  private UserDTO userDTO;
+//
+//  @BeforeEach
+//  void setUp() {
+//    MockitoAnnotations.openMocks(this);
+//
+//    mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
+//
+//    userId = UUID.randomUUID();
+//    userDTO = new UserDTO(userId, "Test User");
+//  }
+//
+//  @Test
+//  void testFindAllUsers() throws Exception {
+//    when(userService.findAll()).thenReturn(List.of(userDTO));
+//
+//    mockMvc.perform(get("/users"))
+//        .andExpect(status().isOk())
+//        .andExpect(jsonPath("$.data[0].id").value(userDTO.id().toString()))
+//        .andExpect(jsonPath("$.data[0].name").value(userDTO.name()));
+//  }
+//
+//  @Test
+//  void testSaveUser() throws Exception {
+//    when(userService.save(anyString(), anyString())).thenReturn(java.util.Optional.of(userDTO));
+//
+//    mockMvc.perform(post("/users")
+//            .contentType(MediaType.APPLICATION_JSON)
+//            .content("{\"name\":\"Test User\",\"password\":\"password123\"}"))
+//        .andExpect(status().isCreated())
+//        .andExpect(jsonPath("$.data.name").value("Test User"));
+//
+//    verify(userService, times(1)).save(anyString(), anyString());
+//  }
+//
+//  @Test
+//  void testSaveUserBadRequest() throws Exception {
+//    when(userService.save(any(), any())).thenReturn(Optional.empty());
+//
+//    mockMvc.perform(post("/users")
+//            .contentType(MediaType.APPLICATION_JSON)
+//            .content("{\"name\":\"Test User\",\"password\":\"password123\"}"))
+//        .andExpect(status().isBadRequest())
+//        .andExpect(jsonPath("$.message").value("Не удалось создать пользователя"));
+//  }
+//
+//
+//  @Test
+//  void testGetUserById() throws Exception {
+//    when(userService.getById(userId)).thenReturn(Optional.of(userDTO));
+//
+//    mockMvc.perform(get("/users/{userId}", userId))
+//        .andExpect(status().isOk())
+//        .andExpect(jsonPath("$.data.id").value(userDTO.id().toString()))
+//        .andExpect(jsonPath("$.data.name").value(userDTO.name()));
+//  }
+//
+//  @Test
+//  void testGetUserByIdNotFound() throws Exception {
+//    when(userService.getById(userId)).thenReturn(Optional.empty());
+//
+//    mockMvc.perform(get("/users/{userId}", userId))
+//        .andExpect(status().isNotFound())
+//        .andExpect(jsonPath("$.message").value("Пользователь не найден"));
+//  }
+//
+//
+//  @Test
+//  void testFindUserByName() throws Exception {
+//    when(userService.findByName("Test User")).thenReturn(Optional.of(userDTO));
+//
+//    mockMvc.perform(get("/users/by-name?name=Test User"))
+//        .andExpect(status().isOk())
+//        .andExpect(jsonPath("$.data.id").value(userDTO.id().toString()))
+//        .andExpect(jsonPath("$.data.name").value(userDTO.name()));
+//  }
+//
+//  @Test
+//  void testGetUserByNameNotFound() throws Exception {
+//    when(userService.findByName("NonExistingUser")).thenReturn(java.util.Optional.empty());
+//
+//    mockMvc.perform(get("/users/by-name?name=NonExistingUser"))
+//        .andExpect(status().isNotFound())
+//        .andExpect(jsonPath("$.message").value("Пользователь не найден"));
+//
+//    verify(userService, times(1)).findByName("NonExistingUser");
+//  }
+//
+//  @Test
+//  void testUserExists() throws Exception {
+//    when(userService.exists("Test User")).thenReturn(true);
+//
+//    mockMvc.perform(get("/users/exists?name=Test User"))
+//        .andExpect(status().isOk())
+//        .andExpect(jsonPath("$.data").value(true));
+//  }
+//
+//  @Test
+//  void testUserNotExists() throws Exception {
+//    when(userService.exists("Nonexistent User")).thenReturn(false);
+//
+//    mockMvc.perform(get("/users/exists?name=Nonexistent User"))
+//        .andExpect(status().isOk())
+//        .andExpect(jsonPath("$.data").value(false));
+//  }
+//}
