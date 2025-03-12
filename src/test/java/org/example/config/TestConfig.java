a4326f19b9381fa2c39cd312ca8f1ac4bbb31c6f
ivanmikhaylov1 <ivanmikhaylov123@gmail.com>
2025-03-12 14:30:19 +0300

    Исправлена ошибка с тестами

--- src/test/java/org/example/config/TestConfig.java
+++ src/test/java/org/example/config/TestConfig.java
@@ -1,20 +1,18 @@
 package org.example.config;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @TestConfiguration
 public class TestConfig {
   @Bean
-  public DataSource dataSource() {
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setDriverClassName("org.postgresql.Driver");
-    dataSource.setUrl("jdbc:postgresql://localhost:5432/mydb");
-    dataSource.setUsername("postgres");
-    dataSource.setPassword(System.getenv("POSTGRES_PASSWORD"));
-    return dataSource;
+  @ServiceConnection
+  public PostgreSQLContainer<?> postgreSQLContainer() {
+    return new PostgreSQLContainer<>("postgres:latest")
+        .withDatabaseName("testdb")
+        .withUsername("test")
+        .withPassword("test");
   }
 }
